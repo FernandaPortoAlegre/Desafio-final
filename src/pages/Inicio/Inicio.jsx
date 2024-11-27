63b7fd1739203816d0bf49ee66a2aef467383ce7
fernanda porto alegre <portoalegrenanda@gmail.com>
2024-11-27 10:40:55 -0300

    atualizando o footer e criando o queroDoar

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,4 +1,8 @@
 import S from './inicio.module.scss'
+import read from '../../assets/reading.png'
+import balance from '../../assets/balance.png'
+import community from '../../assets/community.png'
+import transform from '../../assets/transform.png'
 
 
 export default function Inicio(){
@@ -7,6 +11,29 @@ export default function Inicio(){
             <section className={S.post}>
                 <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
             </section>
+        <section className={S.devoDoar}>
+        <div>
+            <h2>Por que devo Doar</h2>
+        </div>
+        <section className={S.boxCard}>
+          <article>
+            <img src={community} alt="" />
+            <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
+          </article>
+          <article>
+            <img src={read} alt="" />
+            <p> Estimula o hábito da leitura e o aprendizado contínuo.</p>
+          </article>
+          <article>
+            <img src={transform} alt="" />
+            <p> Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.</p>
+          </article>
+          <article>
+            <img src={balance} alt="" />
+            <p> Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>
+          </article>
         </section>
+      </section>
+    </section>
     )
 }
